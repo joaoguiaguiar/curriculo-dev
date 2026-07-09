160289c256dc5a0e1bad33eaf9d9cb868a4ce782
joaoguiaguiar <joaoguiaguiar05@gmail.com>
2026-07-09 17:47:52 -0300

    ajuste banner mobile

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,15 @@
 import styles from './Banner.module.scss';
 import logo__banner from './banner.png';
+import mobileBanner from './banner-mobile.png';
 
 const Banner = () => {
     return (
         <section
             className={styles.banner}
-            style={{ backgroundImage: `url(${logo__banner})` }}
+            style={{
+                '--banner-desktop': `url(${logo__banner})`,
+                '--banner-mobile': `url(${mobileBanner})`,
+            }}
         >
             <div className={styles.apresentacao}>
                 <h1 className={styles.titulo}>Hello World</h1>
